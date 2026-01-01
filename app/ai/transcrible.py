a5f449464a8dec5c8adf51e8413410e42ac50d6e
KrzysztofZakrzewski <krzysztof.zakrzewski@protonmail.com>
2026-01-01 20:44:51 +0100

    created config file with models, change variable model name

--- app/ai/transcrible.py
+++ app/ai/transcrible.py
@@ -1,22 +1,7 @@
 from io import BytesIO
 
-
 # --- AI Model ---
-AUDIO_TRANSCRIBE_MODEL = "whisper-1"
-
-
-# def transcribe_audio_to_words(audio_bytes, language=st.session_state['language_iso']):
-#     openai_client = get_openai_client()
-#     audio_file = BytesIO(audio_bytes)
-#     audio_file.name = "audio.mp3"
-#     transcript = openai_client.audio.transcriptions.create(
-#         file=audio_file,
-#         model=AUDIO_TRANSCRIBE_MODEL,
-#         language=language,
-#         response_format="srt",  # tu se mogę zmienić na SRT ale trzeba usunąć TEXT z RETURNA I NA DOLE rzzy zapisie zamias txt też SRT !!!!! ONEONEONE
-#     )
-
-#     return transcript
+from config.settings import AUDIO_AI_MODEL_WHISPER_1
 
 def transcribe_audio_to_words(audio_bytes, openai_client, language, response_format):
     openai_client=openai_client
@@ -24,7 +9,7 @@ def transcribe_audio_to_words(audio_bytes, openai_client, language, response_for
     audio_file.name = "audio.mp3"
     transcript = openai_client.audio.transcriptions.create(
         file=audio_file,
-        model=AUDIO_TRANSCRIBE_MODEL,
+        model=AUDIO_AI_MODEL_WHISPER_1,
         language=language,
         response_format=response_format,
     )
---
+++ app/config/settings.py
@@ -0,0 +1,6 @@
+# ======================
+# AI MODELS
+# ======================
+
+# --- whisper-1 ---
+AUDIO_AI_MODEL_WHISPER_1 = "whisper-1"
